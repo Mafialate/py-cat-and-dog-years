35aa1c4402243975ff49b86cc1b135f0442f8f1b
Mafialate <mafialate@ukr.net>
2026-02-01 18:36:39 +0100

    Add convert_age and test_get_human_age_wrong_values functions

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 import math
 
 
+def convert_age(age: int, animal_to_human_ratio: int) -> int:
+    if age < 15:
+        return 0
+    elif age < 24:
+        return 1
+    else:
+        return math.floor((age - 24) / animal_to_human_ratio) + 2
+
+
 def get_human_age(cat_age: int, dog_age: int) -> list[int]:
-    result = []
-    animal_to_human_ratio = 4
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError
+    if cat_age <= 0 or cat_age > 100:
+        raise ValueError
+    if dog_age <= 0 or dog_age > 100:
+        raise ValueError
 
-    for age in cat_age, dog_age:
-        if age < 15:
-            result.append(0)
-        elif age < 24:
-            result.append(1)
-        else:
-            human_age = math.floor((age - 24) / animal_to_human_ratio) + 2
-            result.append(human_age)
-            animal_to_human_ratio += 1
+    cat_to_human_age = convert_age(cat_age, 4)
+    dog_to_human_age = convert_age(dog_age, 5)
 
-    return result
+    return [cat_to_human_age, dog_to_human_age]
--- app/test_main.py
+++ app/test_main.py
@@ -6,8 +6,6 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
-        (0, 0, [0, 0]),
-        (-1, -1, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
@@ -15,8 +13,6 @@ from app.main import get_human_age
         (100, 100, [21, 17])
     ],
     ids=[
-        "test_returns_zero_when_age_is_zero",
-        "test_returns_zero_when_age_is_negative",
         "test_returns_zero_when_age_below_first_threshold_15_years",
         "test_returns_one_when_age_equals_first_threshold_15_years",
         "test_returns_one_when_age_between_thresholds_15_and_24_years",
@@ -24,5 +20,35 @@ from app.main import get_human_age
         "test_returns_expected_values_for_extreme_ages",
     ]
 )
-def test_get_human_age(cat_age: int, dog_age: int, expected: str) -> None:
+def test_get_human_age_right_values(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (1, 0, ValueError),
+        (0, 1, ValueError),
+        (0, 0, ValueError),
+        (-1, -1, ValueError),
+        ([], -1, TypeError),
+    ],
+    ids=[
+        "test_returns_value_error_when_first_value_is_zero",
+        "test_returns_value_error_when_second_value_is_zero",
+        "test_returns_value_error_when_all_values_is_zero",
+        "test_returns_value_error_when_value_is_negative",
+        "test_returns_type_error_when_value_is_not_int"
+    ]
+)
+def test_get_human_age_wrong_values(
+        cat_age: int,
+        dog_age: int,
+        expected: TypeError | ValueError
+) -> None:
+    with pytest.raises(expected):
+        get_human_age(cat_age, dog_age)
